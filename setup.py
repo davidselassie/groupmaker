33ea9289d230f5825910efd2c64daa1cb149db45
David Selassie <selassid@gmail.com>
2016-09-16 14:02:02 -0700

    Bumps version to 0.2.0.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='groupmaker',
-    version='0.1.0',
+    version='0.2.0',
     description='Command line tool for making classroom groups.',
     url='https://github.com/selassid/groupmaker',
     author='David Selassie',
